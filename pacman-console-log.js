085fe8c95fe9ef0f3bbb810fd6028b1af3b04d9f
mikesmall <michael.l.a.small@gmail.com>
2017-06-29 16:33:36 -0400

    Completed steps Node.js, JavaScript, and Comments

---
+++ pacman-console-log.js
@@ -0,0 +1,12 @@
+console.log('Pac-Man eats ghosts for lunch.');
+
+// I am a comment!
+var ghost = "Blinky!"; // <<-- this variable assignment is not a comment, but everything after the // in this sentence is a comment.
+/*
+  Each
+  of
+  theses
+  lines
+  are
+  comments
+*/
